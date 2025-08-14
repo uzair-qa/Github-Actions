f474265f9916e0b2ea1c3c5f3b9bff2fd835a845
Uzair Khan <khanuzair54670553@agentforce.com>
2025-08-14 14:51:50 +0530

    first commit

---
+++ tests/example.spec.ts
@@ -0,0 +1,17 @@
+import { test, expect } from '@playwright/test';
+
+test('Test1', async ({ page }) => {
+  console.log('Test 1');
+});
+
+test('Test2', async ({ page }) => {
+  console.log('Test 2');
+});
+
+test('Test3', async ({ page }) => {
+  console.log('Test 3');
+});
+
+test('Test4', async ({ page }) => {
+  console.log('Test 4');
+});
---
+++ tests/example2.spec.ts
@@ -0,0 +1,17 @@
+import { test, expect } from '@playwright/test';
+
+test('Test5', async ({ page }) => {
+  console.log('Test 5');
+});
+
+test('Test6', async ({ page }) => {
+  console.log('Test 6');
+});
+
+test('Test7', async ({ page }) => {
+  console.log('Test 7');
+});
+
+test('Test8', async ({ page }) => {
+  console.log('Test 8');
+});
